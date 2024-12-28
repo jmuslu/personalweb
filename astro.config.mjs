e6beaafa4a5e7480598be3dee847cbcdd807940f
jbmus <jbmuslu@hotmail.com>
2024-12-28 17:38:20 -0500

    moving files around

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
     site: '/<https://jmuslu.github.io/personalweb/>/',
+    outDir: './dist',
     base: '/<personalweb>/',
     output: 'static',
+    build: {
+        assets: '_assets'
+    }
 });
