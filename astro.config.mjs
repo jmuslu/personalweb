69ae58aaac438710725e50f019c4811d034e51be
jbmus <jbmuslu@hotmail.com>
2024-12-28 19:57:52 -0500

    i dont even know anymore

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
     site: 'https://jmuslu.github.io',
-    base: '/personalweb',
+
     outDir: './dist',
     output: 'static',
     build: {
